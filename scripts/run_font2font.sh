e8745ee0b082557aba7ce89ab9173b6d4027b8dd
YCHM0304 <samsamsam876@gmail.com>
2024-04-22 16:40:04 +0800

    add run_font2font scipt

---
+++ scripts/run_font2font.sh
@@ -0,0 +1,9 @@
+python font2img.py --src_font ./charset/src/PMingLiU-02.ttf \
+                   --dst_font ./charset/dst/DFHsiu-W3-WIN-BF-01.ttf \
+                   --charset CN \
+                   --sample_count 1000 \
+                   --sample_dir ./examples \
+                   --label 0 \
+                   --filter \
+                   --shuffle \
+                   --mode font2font
